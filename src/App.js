783c4b02e8a16d1d7f04e2cda599f420a67c37c2
shai2h <sweden.fan@yandex.ru>
2022-01-12 07:11:31 +0300

    Main block
    Вёрстка главной страницы

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+// import Slider from 'react-slick';
+import React, { Component } from "react";
+import Slider from './components/main-slider/slider'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header className="header">
+        <span className="logo">LOGO</span>
+        <nav className="nav-items">
+          <ul className="ul-items">
+            <li>О нас</li>
+            <li>Услуги</li>
+            <li>Портфолио</li>
+            <li>Награды</li>
+          </ul>
+        </nav>
+        <span className="phone">+800 000 0000</span>      
       </header>
+
+      <div className="main-info">
+        <div className="main-slider">
+          <Slider></Slider>
+        </div>
+      </div>
+
+      <div className="about-us">
+        <div className="about-us-text">
+          <h1>
+            Кто мы ?
+          </h1>
+          <p>The word ‘architecture’ has Greek roots. It means ‘the principal builder’. The style of architecture changes according to latest technical features and people’s tastes. Some people think that architecture is simply the outer look of the building. But it’s not the full notion for this complex art form. Architecture includes designing and constructing the buildings as well as building them. Being an architect is not an easy job. A good architect should think over many tiny details before planning his project. </p>
+          <h5>Подробнее<span></span></h5>
+        
+        
+        
+        </div>
+      </div> 
+
+
     </div>
   );
 }
---
+++ src/components/main-slider/slider.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import './main-slider.scss';
+
+
+
+
+
+export default class SimpleSlider extends Component {
+    render() {
+      const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+      return (
+        <div>
+          <Slider {...settings}>
+            <div className="slide1">
+              <div className="count">
+                <span>01</span>/03
+              </div>
+              <div className="first-block">
+                <h1>Архитектурное проектирование</h1>
+                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+                  Velit officia consequat duis enim velit mollit. 
+                  Exercitation veniam consequat sunt nostrud amet.</p>
+                  <button className="first-block-btn">
+                    Оставить заявку
+                  </button>
+              </div>
+              <div className="mail-svg"></div>
+              <div className="inst-svg"></div>
+
+            </div>
+            
+            <div className="slide1">
+              <div className="count">
+                <span>02</span>/03
+              </div>
+              <div className="first-block">
+                <h1>СЛАЙД НОМЕР ДВА</h1>
+                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+                  Velit officia consequat duis enim velit mollit. 
+                  Exercitation veniam consequat sunt nostrud amet.</p>
+                  <button className="first-block-btn">
+                    Оставить заявку
+                  </button>
+              </div>
+            </div>
+            
+            <div className="slide1">
+              <div className="count">
+                <span>03</span>/03
+              </div>
+              
+              <div className="first-block">
+                <h1>СЛАЙД НОМЕР ТРИ</h1>
+                <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
+                  Velit officia consequat duis enim velit mollit. 
+                  Exercitation veniam consequat sunt nostrud amet.</p>
+                  <button className="first-block-btn">
+                    Оставить заявку
+                  </button>
+              </div>
+            </div>        
+          </Slider>
+        </div>
+      );
+    }
+  }
+
+
