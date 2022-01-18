8d9e39e65b68dba7ecf8da5666b130b5ab22c0cd
Ilfat Shaydullin <sweden.fan@yandex.ru>
2022-01-18 10:09:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 // import Slider from 'react-slick';
-import React, { Component } from "react";
-import Slider from './components/main-slider/slider'
+import react from "react";
+import Slider from './components/main-slider/slider';
 
 
 
