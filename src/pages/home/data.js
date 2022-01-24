b0a0e24be9aa73272104912c1993d37d92569908
Ilfat Shaydullin <sweden.fan@yandex.ru>
2022-01-24 20:15:49 +0300

    update

---
+++ src/pages/home/data.js
@@ -0,0 +1,74 @@
+import card1 from '../home/main_page_img/projects_block_card/card1.png'
+import card2 from '../home/main_page_img/projects_block_card/card2.png'
+import card3 from '../home/main_page_img/projects_block_card/card3.png'
+import card4 from '../home/main_page_img/projects_block_card/card4.png'
+
+const data = {
+    cardData:[
+        {
+            id: 1,
+            img: card1,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 2,
+            img: card2,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 3,
+            img: card3,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 4,
+            img: card4,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 5,
+            img: card1,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 6,
+            img: card2,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 7,
+            img: card3,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 8,
+            img: card4,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 9,
+            img: card1,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        {
+            id: 10,
+            img: card2,
+            title: 'Название проекта',
+            link: 'Подробнее →'
+        },
+        
+
+        
+    ]
+}
+
+export default data
